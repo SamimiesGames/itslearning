d596c1538e70b971c9fe8062249716bfd1f958e1
SamimiesGames <sami.laatikainen@saunalahti.fi>
2021-09-13 10:26:33 +0300

    kolmio assignment complete.

---
+++ osa-4/2/kolmio.py
@@ -0,0 +1,24 @@
+# cant declare import from viiva because TMC doesn't support my ".idea" environment.
+
+
+def viiva(width, string):
+    empty_char_fill = "*"  # TMC forces this bad practice.
+
+    is_empty_char = bool(string)
+
+    if is_empty_char:
+        first_character = string[0]
+        print(first_character * width)
+    else:
+        print(empty_char_fill * width)
+
+
+def kolmio(size):
+    for index in range(size):
+        width = index + 1
+
+        viiva(width, "#")
+
+
+if __name__ == '__main__':
+    kolmio(10)
